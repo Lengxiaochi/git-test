5f0237135d63dec04d00ae56163bc1d4c40753d8
lxc <1312219301@qq.com>
2022-07-27 00:54:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Lin Hongyu
+ * @create 2022-07-27-0:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
